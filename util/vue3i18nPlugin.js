904241e01240d06ebcfad493cd8e0eb6c406ed91
lilaobiao <46508106+lilaobiao@users.noreply.github.com>
2020-08-07 16:48:29 +0800

    1
    1

---
+++ util/vue3i18nPlugin.js
@@ -0,0 +1,24 @@
+import { ref, provide, inject } from "vue";
+
+const createI18n = config => ({
+  locale: ref(config.locale), // ref创建一个响应式对象
+  messages: config.messages,
+  $t(key) {
+    return this.messages[this.locale.value][key];
+  }
+});
+
+const i18nSymbol = Symbol();
+
+export function provideI18n(i18nConfig) {
+  const i18n = createI18n(i18nConfig);
+  // 向下层组件提供 i18n 对象
+  provide(i18nSymbol, i18n);
+}
+
+export function useI18n() {
+  const i18n = inject(i18nSymbol);
+  if (!i18n) throw new Error("No i18n provided!!!");
+
+  return i18n;
+}
