4b005a17d8b8a7e5c81e45f7861c3755b3b06978
lilaobiao <46508106+lilaobiao@users.noreply.github.com>
2021-11-02 21:58:17 +0800

    Update parseproto.js

--- parseproto.js
+++ parseproto.js
@@ -18,13 +18,13 @@ const message = {};
 
 let inService = false;
 let inMethod = false;
+let currentMethodName = '';
 
 let inEnum = false;
-let enumStack = [];
+let currentEnumName = '';
 
 let inMessage = false;
 let messageStack = [];
-// let messageObjStack = [];
 let currentMessage = {};
 
 
@@ -32,6 +32,7 @@ let currentMessage = {};
 rl.on('line', (line) => {
     var str = line.trim();
     if(str !== '' && !str.startsWith('//')){
+        // console.log(str);
         // 去除注释部分
         if(str.includes('//')){
             str = str.split('//')[0].trim();
@@ -61,14 +62,28 @@ rl.on('line', (line) => {
                     reqData: req,
                     resData: res
                 }
+                currentMethodName = method;
                 inMethod = true;
+            }
+        }
+        else if(/^option\s*\(srpc.(service|method)_option_id\)\s*=\s*0x\d+\s*;$/.test(str)){
+            // option (srpc.service_option_id) = 0x9999999;
+            // option (srpc.method_option_id) = 0x1;
+            const value = str.split('=')[1].split(';')[0].trim();
+            // 先满足method
+            if(inMethod){
+                if(service[currentMethodName]){
+                    service[currentMethodName].methodId = value;
+                }
+            }else if(inService){
+                proto.serviceId = value;
             }
         }
         // enum
         else if(/^enum\s+\w+\s+{$/.test(str)){
             // enum Market {
             const enumName = str.slice(4).replace('{', '').trim();
-            enumStack.push(enumName);
+            currentEnumName = enumName;
             enums[enumName] = {};
             inEnum = true;
         }
@@ -84,7 +99,7 @@ rl.on('line', (line) => {
                 if(!isNaN(+value)){
                     value = +value;
                 }
-                const enumName = enumStack[enumStack.length - 1];
+                const enumName = currentEnumName;
                 enums[enumName][name] = value;
             }
         }
@@ -112,11 +127,12 @@ rl.on('line', (line) => {
             let text = str.slice(0, -1).trim().replace(/\s+/g, ' ');//optional bool is_month = 2
             text = text.split('=')[0].trim();//optional bool is_month
             let [optional, dataType, key] = text.split(' ');
-            if(['uinit32', 'uinit64', 'init32', 'init64'].includes(dataType)){
+            if(['uint32', 'uint64', 'int32', 'int64'].includes(dataType)){
                 dataType = 'number';
             }else if(dataType === 'bool'){
                 dataType = 'boolean'
             }
+            key = key.replace(/(_[a-z])/g, $1=>$1.slice(1).toUpperCase());
             currentMessage[key] = {
                 type: dataType,
                 optional: optional,
@@ -124,12 +140,13 @@ rl.on('line', (line) => {
         }
         else if(str === '}'){
             if(inMethod){
+                currentMethodName = '';
                 inMethod = false;
             }else if(inService){
                 inService = false;
             }else if(inEnum){
                 inEnum = false;
-                enumStack.pop();
+                currentEnumName = '';
             }else if(inMessage){
                 // message存在嵌套的情况
                 if(messageStack.length === 1){
@@ -152,10 +169,198 @@ rl.on('line', (line) => {
 });
 
 rl.on('close', (line) => {
-//   console.log("读取完毕！");
-//   console.log('proto', proto);
-//   console.log('service', service);
-//   console.log('enum', enums);
-//   console.log('data', data);
-    console.log('message', JSON.stringify(message));
+    console.log("读取完毕！");
+    proto.methods = service;
+    proto.enums = enums;
+    proto.messages = message;
+    console.log('message', JSON.stringify(proto));
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+第二部分
+
+"GetDailyRemittanceRsp": {
+            "innerMessages": {
+                "RemittanceAmount": {
+                    "innerMessages": {
+                        "RemittanceAmount2": {
+                            "market": {
+                                "type": "number",
+                                "optional": "optional"
+                            },
+                            "buyCash": {
+                                "type": "string",
+                                "optional": "optional"
+                            },
+                            "remittanceAmount_3": {
+                                "type": "RemittanceAmount3",
+                                "optional": "optional"
+                            },
+                            "remittanceAmount_4": {
+                                "type": "RemittanceAmount4",
+                                "optional": "optional"
+                            },
+                            "innerMessages": {
+                                "RemittanceAmount3": {
+                                    "market": {
+                                        "type": "number",
+                                        "optional": "optional"
+                                    },
+                                    "buyCash": {
+                                        "type": "string",
+                                        "optional": "optional"
+                                    },
+                                    "tradeDate": {
+                                        "type": "string",
+                                        "optional": "optional"
+                                    },
+                                    "remitDate": {
+                                        "type": "string",
+                                        "optional": "optional"
+                                    }
+                                },
+                                "RemittanceAmount4": {
+                                    "market": {
+                                        "type": "number",
+                                        "optional": "optional"
+                                    },
+                                    "buyCash": {
+                                        "type": "string",
+                                        "optional": "optional"
+                                    },
+                                    "tradeDate": {
+                                        "type": "string",
+                                        "optional": "optional"
+                                    },
+                                    "remitDate": {
+                                        "type": "string",
+                                        "optional": "optional"
+                                    }
+                                }
+                            }
+                        },
+                        "RemittanceAmount5": {
+                            "market": {
+                                "type": "number",
+                                "optional": "optional"
+                            },
+                            "buyCash": {
+                                "type": "string",
+                                "optional": "optional"
+                            },
+                            "tradeDate": {
+                                "type": "string",
+                                "optional": "optional"
+                            },
+                            "remitDate": {
+                                "type": "string",
+                                "optional": "optional"
+                            }
+                        }
+                    },
+                    "market": {
+                        "type": "number",
+                        "optional": "optional"
+                    },
+                    "buyCash": {
+                        "type": "string",
+                        "optional": "optional"
+                    },
+                    "remittanceAmount_2": {
+                        "type": "RemittanceAmount2",
+                        "optional": "optional"
+                    },
+                    "remittanceAmount_5": {
+                        "type": "RemittanceAmount5",
+                        "optional": "optional"
+                    }
+                }
+            },
+            "result": {
+                "type": "number",
+                "optional": "optional"
+            },
+            "errMsg": {
+                "type": "string",
+                "optional": "optional"
+            },
+            "remittances": {
+                "type": "RemittanceAmount",
+                "optional": "repeated"
+            },
+            "date": {
+                "type": "string",
+                "optional": "optional"
+            }
+        },
+/////////////////////////////////////////////////////////////////////////////////////////////
+const mock = require('mockjs');
+const random = mock.Random;
+
+const proto = require('./proto.json');
+// console.log(proto.serviceName, random.cname());
+
+var GetDailyRemittanceRsp = proto.messages.GetDailyRemittanceRsp;
+
+function generateData(message, proto){
+    var innerMessage = message.innerMessages;
+    const data = {}; 
+    Object.keys(message).filter(key => key !== 'innerMessages').forEach(key => {
+        const valueDesc = message[key];
+        let value;
+        switch(valueDesc.type){
+            case 'string':
+                value = random.cword();
+                break;
+            case 'number':
+                value = random.integer();
+                break;
+            case 'boolean':
+                value = random.boolean();
+                break;
+            default:
+                value = generateObjValue(valueDesc, innerMessage, proto);
+        }
+        // 数组
+        if(valueDesc.optional === 'repeated'){
+            data[key] = [value];
+        }else{
+            data[key] = value;
+        }
+    });
+    return data;
+};
+
+
+function generateObjValue(valueDesc, innerMessages, proto){
+    const type = valueDesc.type;
+    console.log(innerMessages, type);
+    if(innerMessages && innerMessages[type]){
+        return generateData(innerMessages[type], proto);
+    }
+    // 避免属性不存在
+    else if(proto[type]){
+        return generateData(proto[type], proto);
+    }else{
+        return 'data type not defined';
+    }
+}
+
+
+console.log(JSON.stringify(generateData(GetDailyRemittanceRsp, proto)));
+
+// Mock.mock('http://www.bai.com',{
+//     'firstName|3':'fei',//重复fei这个字符串 3 次，打印出来就是'feifeifei'。
+//     'lastName|2-5':'jiang',//重复jiang这个字符串 2-5 次。
+//     'big|+1':0, //属性值自动加 1，初始值为 0
+//     'age|20-30':25,//生成一个大于等于 20、小于等于 30 的整数，属性值 25 只是用来确定类型
+//     'weight|100-120.2-5':110.24,//生成一个浮点数,整数部分大于等于 100、小于等于 120，小数部分保留 2 到 5 位。
+//     'likeMovie|1':Boolean,//随机生成一个布尔值，值为 true 的概率是 1/2，值为 false 的概率同样是 1/2。
+//     'friend1|1':arr,//从数组 arr 中随机选取 1 个元素，作为最终值。
+//     'friend2|+1':arr,//从属性值 arr 中顺序选取 1 个元素，作为最终值
+//     'friend3|2-3':arr,//通过重复属性值 arr 生成一个新数组，重复次数大于等于 2，小于等于 3。
+//     'life1|2':obj,//从属性值 obj 中随机选取 2 个属性
+//     'life1|1-2':obj,//从属性值 obj 中随机选取 1 到 2 个属性。
+//     'regexp1':/^[a-z][A-Z][0-9]$/,//生成的符合正则表达式的字符串
+// })
